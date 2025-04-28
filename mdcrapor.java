92e9b7029f9a2b43095e7d09a9c3a8994b415394
Murat <72228039+muraat0@users.noreply.github.com>
2025-04-28 11:44:50 +0300

    mdcrapor.java

---
+++ mdcrapor.java
@@ -0,0 +1,13 @@
+package Algoritmafinal;
+
+public class mdcrapor
+{
+    String satir;
+    String date;
+    String lokasyon;
+    String raporu_yazan;
+    public String toString()
+    {
+        return "Tarih: " + date + "\nOlay: " + satir + " | Lokasyon: " + lokasyon;
+    }
+}
